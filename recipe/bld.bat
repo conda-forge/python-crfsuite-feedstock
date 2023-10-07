de5076335902840f1d819973519a949555ec6530
Uwe L. Korn <uwelk@xhochy.com>
2023-10-07 21:19:36 +0200

    Different echo syntax

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ pushd pycrfsuite
 del _pycrfsuite.cpp
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "# distutils: language = c++" > _pycrfsuite.pyx.tmp
+(echo # distutils: language = c++) > _pycrfsuite.pyx.tmp
 type _pycrfsuite.pyx >> _pycrfsuite.pyx.tmp
 move _pycrfsuite.pyx.tmp _pycrfsuite.pyx
 cythonize _pycrfsuite.pyx

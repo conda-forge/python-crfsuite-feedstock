df037e50a5cc5609af8c7bb5bb5a8d677452dc16
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-10-05 11:53:50 -0500

    use shellcheck

--- recipe/build-pycrfsuite.bat
+++ recipe/build-pycrfsuite.bat
@@ -2,16 +2,12 @@
 
 pushd pycrfsuite
 
-del _pycrfsuite.cpp
-if %errorlevel% neq 0 exit /b %errorlevel%
-
+del _pycrfsuite.cpp || exit 2
 (echo # distutils: language = c++) > _pycrfsuite.pyx.tmp
 type _pycrfsuite.pyx >> _pycrfsuite.pyx.tmp
 move _pycrfsuite.pyx.tmp _pycrfsuite.pyx
-cythonize _pycrfsuite.pyx
-if %errorlevel% neq 0 exit /b %errorlevel%
+cythonize _pycrfsuite.pyx || exit 3
 
 popd
 
-%PYTHON% -m pip install -vvv .
-if %errorlevel% neq 0 exit /b %errorlevel%
+"%PYTHON%" -m pip install -vvv . --no-deps --no-build-isolation --disable-pip-version-check || exit 4
